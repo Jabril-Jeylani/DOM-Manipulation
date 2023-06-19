f88d566e526692973842130aea154278d1fd8b3d
Jabril-Jeylani <jabrilmj@gmail.com>
2023-06-19 12:48:56 -0600

    finished

--- script.js
+++ script.js
@@ -17,31 +17,65 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  const mainTitleId = document.querySelector('#main-title')
+  mainTitleId.innerText = 'DOM Toretto'
 
   // Part 2
-
+  const body = document.querySelector('body')
+  this.body.style.backgroundColor = 'pink'
 
   // Part 3
-
+  const favoriteThings = document.querySelector('#favorite-things')
+  let lastChild = favoriteThings.lastElementChild 
+  favoriteThings.removeChild(lastChild)
 
   // Part 4
-
+  const specialTitle = document.querySelectorAll('.special-title')
+  for(i of specialTitle){
+    i.style.fontSize = '2rem';
+  }
 
   // Part 5
-
+  const pastRaces = document.querySelectorAll('.pastRaces')
+  pastRaces[3].innerHTML = ''
+  
 
   // Part 6
-
+  const liElement = document.createElement('li')
+  liElement.innerHTML = 'Riyadh'
+  document.getElementById('past-races').appendChild(liElement)
 
   // Part 7
+  const blogPost = document.createElement('div')
+  blogPost.className = 'blog-post'
+  const h2Element = document.createElement('h2')
+  h2Element.textContent = 'Riyadh'
+  const pElement = document.createElement('p')
+  pElement.textContent = 'I loved the Riyadh Highways'
 
+  const main = document.querySelector('.main')
+  main.appendChild(blogPost)
+  blogPost.appendChild(h2Element)
+  h2Element.appendChild(pElement)
 
+  blogPost.style.backgroundColor = 'rebeccaPurple'
+  
   // Part 8
-
+  const quote = document.querySelector('#quote-title')
+  quote.addEventListener('click', randomQuote)
 
   // Part 9
-
+  const blogEl = document.getElementsByClassName('blog-post')
+  console.log(blogEl)
+  for (let i = 0; i < blogEl.length; i++){
+    blogEl[i].addEventListener('mouseenter', function(){
+      blogEl[i].classList.toggle('red')
+    })
+    blogEl[i].addEventListener('mouseout', function(){
+      blogEl[i].classList.toggle('purple')
+    })
+    
+  }
 
 
 
